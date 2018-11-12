8fb31984a3f5b15ed1723a6941c3fc996079bdc3
daylight <479428129@qq.com>
2018-11-13 00:15:49 +0800

    rename classes

--- web/src/main/java/group/ydq/web/WebApplication.java
+++ web/src/main/java/group/ydq/web/WebApplication.java
@@ -2,8 +2,10 @@ package group.ydq.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@SpringBootApplication(scanBasePackages = "group.ydq")
 public class WebApplication {
 
 	public static void main(String[] args) {
