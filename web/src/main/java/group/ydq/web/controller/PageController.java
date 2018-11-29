e0a516e0c6017d192ff8856e80f4ca1eb17b7708
simple <2371183243@qq.com>
2018-11-29 19:48:19 +0800

    add messageTest page

--- web/src/main/java/group/ydq/web/controller/PageController.java
+++ web/src/main/java/group/ydq/web/controller/PageController.java
@@ -10,17 +10,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
     @RequestMapping("/")
-    public String index(){
+    public String index() {
         return "home.html";
     }
 
     @RequestMapping("/login")
-    public String login(){
+    public String login() {
         return "login.html";
     }
 
     @RequestMapping("/register")
-    public String register(){
+    public String register() {
         return "register.html";
+    }
+
+    @RequestMapping("/message")
+    public String message() {
+        return "messageTest.html";
     }
 }
