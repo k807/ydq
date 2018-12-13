6d7b5e17215f477adda568644138b8df4c1afa6d
Leo <mmm.leo@outlook.com>
2018-12-13 17:30:29 +0800

    fix some bugs

--- web/src/main/java/group/ydq/web/controller/StageController.java
+++ web/src/main/java/group/ydq/web/controller/StageController.java
@@ -1,7 +1,17 @@
 package group.ydq.web.controller;
 
+import group.ydq.model.dto.BaseResponse;
+import group.ydq.model.entity.cs.CheckStage;
+import group.ydq.model.entity.dm.Project;
+import group.ydq.service.service.CheckStageService;
+import group.ydq.service.service.FileService;
+import group.ydq.service.service.ProjectService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
 
 /**
  * author:Leo
@@ -10,46 +20,46 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/stage")
 public class StageController {
-//    @Resource
-//    private ProjectService projectService;
-//
-//    @Resource
-//    private FileService fileService;
-//
-//    @Resource
-//    private CheckStageService checkStageService;
-//
-//    @RequestMapping("/startMid")
-//    private BaseResponse startMid(long projectId){
-//        CheckStage cs = new CheckStage();
-//        Project p = new Project();
-//        p = projectService.getProject(projectId);
-//        cs.setProject(p);
-//        cs.setStartTime(new Date());
-//        cs.setUploadStatus(false);
-//        cs.setStage(1);
-//        cs.setSatus(0);
-//        checkStageService.save(cs);
-//        return new BaseResponse();
-//    }
-//
-//    @RequestMapping("/startFinal")
-//    private BaseResponse startFinal(long projectId){
-//        CheckStage cs = new CheckStage();
-//        Project p = new Project();
-//        p = projectService.getProject(projectId);
-//        cs.setProject(p);
-//        cs.setStartTime(new Date());
-//        cs.setUploadStatus(false);
-//        cs.setStage(2);
-//        cs.setSatus(0);
-//        checkStageService.save(cs);
-//        return new BaseResponse();
-//    }
-//
-//    @RequestMapping("/getAll")
-//    private BaseResponse getAll(){
-//        List<CheckStage> all =  checkStageService.findAll();
-//        return new BaseResponse();
-//    }
+    @Resource
+    private ProjectService projectService;
+
+    @Resource
+    private FileService fileService;
+
+    @Resource
+    private CheckStageService checkStageService;
+
+    @RequestMapping("/startMid")
+    private BaseResponse startMid(long projectId){
+        CheckStage cs = new CheckStage();
+        Project p = new Project();
+        p = projectService.getProject(projectId);
+        cs.setProject(p);
+        cs.setStartTime(new Date());
+        cs.setUploadStatus(false);
+        cs.setStage(1);
+        cs.setSatus(0);
+        checkStageService.save(cs);
+        return new BaseResponse();
+    }
+
+    @RequestMapping("/startFinal")
+    private BaseResponse startFinal(long projectId){
+        CheckStage cs = new CheckStage();
+        Project p = new Project();
+        p = projectService.getProject(projectId);
+        cs.setProject(p);
+        cs.setStartTime(new Date());
+        cs.setUploadStatus(false);
+        cs.setStage(2);
+        cs.setSatus(0);
+        checkStageService.save(cs);
+        return new BaseResponse();
+    }
+
+    @RequestMapping("/getAll")
+    private BaseResponse getAll(){
+        List<CheckStage> all =  checkStageService.findAll();
+        return new BaseResponse();
+    }
 }
