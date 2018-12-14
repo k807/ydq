99cc0f1b0a8912828fddf69963b91e4edd18b385
simple <2371183243@qq.com>
2018-12-14 18:59:15 +0800

    message finish!

--- model/src/main/java/group/ydq/model/dao/pm/MessageRepository.java
+++ model/src/main/java/group/ydq/model/dao/pm/MessageRepository.java
@@ -2,7 +2,6 @@ package group.ydq.model.dao.pm;
 
 import group.ydq.model.entity.pm.Message;
 import group.ydq.model.entity.rbac.User;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -18,10 +17,10 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
     @Query(value = "select m from Message m ORDER BY m.date ASC")
     List<Message> findAllOrderByDate();
 
-    @Query(value = "select m from Message m where m.sender = ?1 ORDER BY ?#{#pageable}")
-    List<Message> findBySender(User sender, Pageable pageable);
+    @Query(value = "select m from Message m where m.sender = ?1 ORDER BY m.date ASC")
+    List<Message> findBySender(User sender);
 
 
-    @Query(value = "select m from Message m where m.receiver = ?1 ORDER BY ?#{#pageable}")
-    List<Message> findByReceiver(User receiver, Pageable pageable);
+    @Query(value = "select m from Message m where m.receiver = ?1 ORDER BY m.date ASC")
+    List<Message> findByReceiver(User receiver);
 }
